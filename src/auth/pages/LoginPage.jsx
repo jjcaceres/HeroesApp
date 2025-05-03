d247f6902293612a98226b8841d37fae5e09c47f
Jose Caceres <jose.caceres.mamani@gmail.com>
2025-05-02 22:44:48 -0500

    add route

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { AuthContext } from '../context/AuthContext'
 
 export const LoginPage = () => {
 
+    const { login } = useContext(AuthContext)
+
     const navigate = useNavigate()
     const onLogin = () => {
-        navigate('/', { replace: true })
+
+        const lastPath = localStorage.getItem('lastPath') || '/';
+
+        login('Jose Caceres');
+        navigate(lastPath, { replace: true })
     }
 
     return (
